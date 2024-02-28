cd6391b53da7c2785d0f31610d3dad4d6a3ec9af
precious-appiah <appiahprecious99@gmail.co>
2024-02-28 11:30:13 +0000

    task 1 fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,10 @@
 # this is to install flask from pip
+
+package { 'werkzeug' :
+        ensure  => '2.1.1',
+        provider => 'pip3'
+}
+
 package { 'flask' :
         ensure   => '2.1.0',
         provider =>'pip3'
